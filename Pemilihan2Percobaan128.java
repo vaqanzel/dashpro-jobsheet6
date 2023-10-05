21681511eab15cdef322e8a5685d3d1cc6f73f99
vaqanzel <valengrehasta@gmail.com>
2023-10-05 09:26:46 +0700

    first commit jobsheet 6

---
+++ Pemilihan2Percobaan128.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+public class Pemilihan2Percobaan128 {
+    public static void main(String[] args) {
+        Scanner input28 = new Scanner(System.in);
+
+        System.out.print("Masukkan tahun : ");
+        int tahun = input28.nextInt();
+
+       
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0)
+                System.out.print("Tahun Kabisat");
+        } else         
+            System.out.print("Bukan Tahun Kabisat");
+
+
+
+    }
+}
