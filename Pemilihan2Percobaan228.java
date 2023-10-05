374dfe482e0cda2380a25e62295c544d255e2f4f
vaqanzel <valengrehasta@gmail.com>
2023-10-05 13:12:43 +0700

    modifikasi percobaan 2

--- Pemilihan2Percobaan228.java
+++ Pemilihan2Percobaan228.java
@@ -5,7 +5,8 @@ public class Pemilihan2Percobaan228 {
         Scanner input28 = new Scanner(System.in);
 
         int sudut1, sudut2, sudut3, totalSudut;
-    System.out.print("Masukkan Sudut 1 : ");
+        
+        System.out.print("Masukkan Sudut 1 : ");
         sudut1 = input28.nextInt();
         System.out.print("Masukkan Sudut 2 : ");
         sudut2 = input28.nextInt();
@@ -17,9 +18,14 @@ public class Pemilihan2Percobaan228 {
         if (totalSudut == 180) {
             if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90))
                 System.out.println("Segitiga tersebut adalah segitiga siku-siku"); 
+            else if (sudut1 == sudut2 && sudut2 == sudut3)
+                System.out.println("Segitiga tersebut adalah segitiga sama sisi");
+            else if (sudut1 == sudut2 || sudut2 == sudut3 || sudut1 == sudut3)
+                System.out.println("Segitiga tersebut adalah segitiga sama kaki");
             else
-                System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
-            } else
+                System.out.println("Segitiga tersebut adalah segitiga sembarang");
+        } else
             System.out.println("Bukan Segitiga");
+
     }
 }
