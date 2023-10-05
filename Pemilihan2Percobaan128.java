cbd67c9db4452e773ba7e2e8db67d32f470a0a5d
vaqanzel <valengrehasta@gmail.com>
2023-10-05 10:42:19 +0700

    modifikasi tahun 2100 bukan tahun kabisat

--- Pemilihan2Percobaan128.java
+++ Pemilihan2Percobaan128.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 public class Pemilihan2Percobaan128 {
     public static void main(String[] args) {
@@ -7,13 +6,13 @@ public class Pemilihan2Percobaan128 {
         System.out.print("Masukkan tahun : ");
         int tahun = input28.nextInt();
 
-       
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0)
                 System.out.print("Tahun Kabisat");
+                else 
+                System.out.print("Bukan Tahun Kabisat");
         } else         
             System.out.print("Bukan Tahun Kabisat");
-
 
 
     }
